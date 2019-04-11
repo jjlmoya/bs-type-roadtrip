8a39e038823805814b8809a2434caf761a123344
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-04-11 19:40:59 +0200

    Initial commit modal

---
+++ RoadTrip.php
@@ -0,0 +1,37 @@
+<?php
+
+class RoadTrip
+{
+	private static $instance;
+	public $db;
+	public $singular;
+	public $path;
+	public $plural;
+	public $customFields;
+	public $nameSpace;
+	public $icon;
+
+	public function __construct($singular, $plural, $path, $customFields)
+	{
+		$this->singular = $singular;
+		$this->plural = $plural;
+		$this->db = 'bs-roadtrip';
+		$this->nameSpace = 'bs_roadtrip';
+		$this->path = $path;
+		$this->icon = 'dashicons-chart-line';
+		$this->customFields = $customFields;
+	}
+
+	public static function getInstance($singular, $plural, $path, $customFields)
+	{
+		if (!self::$instance instanceof self) {
+			self::$instance = new self($singular, $plural, $path, $customFields);
+		}
+		return self::$instance;
+	}
+
+}
+
+
+
+
